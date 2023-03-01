6183bc784914821fcb0c60961993d67d74a785f6
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-02-28 22:50:12 -0500

    counter complete exponer metodos del hook

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -11,9 +11,9 @@ export const CounterWithCustomHook = () => {
         <h1>Counter With Hook: {counter}</h1>
         <hr/>
 
-        <button onClick={increment} className="btn btn-primary">+1</button>
+        <button onClick={ () => increment (2)} className="btn btn-primary">+1</button>
         <button onClick={reset} className="btn btn-primary">Reset</button>
-        <button onClick={decrement} className="btn btn-primary">-1</button>
+        <button onClick={() => decrement (2)} className="btn btn-primary">-1</button>
 
     </>
   )
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -5,15 +5,15 @@ export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue)    
 
 
-    const increment =  () => {
-            setCounter(counter + 1 );
+    const increment =  ( value = 1) => {
+            setCounter(counter + value );
     }
 
-    const decrement=  () => {
-        if (counter === 0) return ;
+    const decrement=  (value = 1) => {
+        // if (counter === 0) return ;
 
 
-        setCounter(counter - 1 );
+        setCounter(counter - value);
     }
 
     const reset = () => {
