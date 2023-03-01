a7b3c9558347540c03d4d5b0bac24c5d1640def3
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-02-28 23:28:29 -0500

    precauciones con hooks y la limpieza de ellos

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -5,11 +5,19 @@ export const Message = () => {
 
 
     useEffect(() => {
-      console.log('Message Mounted')
-    
+
+        
+        const onMouseMove = ({x , y} ) => {
+            const coords = {x, y};
+            console.log(coords)
+        }
+
+
+        window.addEventListener('mousemove',onMouseMove);
+
       return () => {
-        console.log('Message Unmounted')
-      }
+        window.removeEventListener('mousemove',onMouseMove);
+    }
     }, [])
     
 
