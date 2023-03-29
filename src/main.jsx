556d910983befc8f71893cc5dc27810b9e3346ae
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-03-28 21:20:31 -0500

    uso de UseLayout Effect hacia un useEffect

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import ReactDOM from 'react-dom/client'
 //import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+//import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
@@ -13,6 +15,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     // <SimpleForm   />
-    <MultipleCustomHooks />
+    <Layout />
   // </React.StrictMode>
 )
