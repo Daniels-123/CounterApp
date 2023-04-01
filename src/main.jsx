a9c00cc2f3e02c745b4eb0bf08e4cb8c50227056
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-03-31 22:22:33 -0500

    Tarea Memorize

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CallBackHook } from './06-memos/CallBackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { Message } from './02-useEffect/Message'
@@ -19,6 +20,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     // <SimpleForm   />
-    <CallBackHook />
+    <Padre />
   // </React.StrictMode>
 )
