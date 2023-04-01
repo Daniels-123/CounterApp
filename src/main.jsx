bbc78e98799a0133d330bc5af661e2b3948a7a20
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-03-31 21:45:12 -0500

    uso de UseMemo

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { Message } from './02-useEffect/Message'
 //import { CounterApp } from './01-useState/CounterApp'
@@ -8,7 +9,8 @@ import ReactDOM from 'react-dom/client'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+//import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
@@ -16,6 +18,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     // <SimpleForm   />
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>
 )
