bf7d4358be7d84993ee304721f3e27b95ae7092d
Daniels-123 <danielbuitragosarmiento@gmail.com>
2023-04-01 17:54:35 -0500

    Resolucion dividida en componentes pequeños

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,5 +1,7 @@
 import React, { useReducer } from 'react'
 import { todoReducer } from './todoReducer';
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 
 const initialState = [
@@ -10,7 +12,7 @@ const initialState = [
     },
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del tiempo',
         donde: false,
     },
 ]
@@ -22,6 +24,11 @@ export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(todoReducer , initialState);
 
+    const handleNewTodo = (todo) => {
+
+        console.log({todo})
+    }
+
 
   return (
     <>
@@ -31,37 +38,19 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className='col-7'>
-                    <ul className='list-group'>
-                        {
-                            todos.map( todo => (
-                                <li className='list-group-item d-flex justify-content-between'>
-                                <span className='align-self-center'>Item 1</span>
-                                <button className='btn btn-danger'> Borrar</button>    
-                                    
-                                </li>
-                            ))
-                        }  
-                    </ul>
+                    { /*TodoList */}
+                    <TodoList todos = { todos}/>
+                    { /*Fin  TodoList */}
                 </div>
 
 
                 <div className="col-5">
                     <h4>Agregar TODO</h4>
                     <hr />
-                    <form >
-                        <input 
-                            type="text"
-                            placeholder='Que hay que hacer?'
-                            className='form-control'
-                         />
-
-                         <button 
-                            type='submit'
-                            className='btn btn-outline-primary mt-1'
-                            >
-                                Agregar
-                            </button>
-                    </form>
+                    {/*TodoAdd onNewTodo (todo ) */}
+                    {/*{id : new Date() ..., description : ''*/}
+                    <TodoAdd onNewTodo={handleNewTodo} />
+                    {/*Fin TodoAdd */}
                 </div>
               
             </div>
